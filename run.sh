870976ca1fa73938666fd20a4fcf6da1b520041d
fermayo <fermayo@gmail.com>
2014-07-19 19:47:13 +0200

    detect fig links

--- run.sh
+++ run.sh
@@ -14,11 +14,22 @@ sed -i "s|{{ SERVER_LRUCACHE_TTL }}|${SERVER_LRUCACHE_TTL}|g" $TEMPLATE
 sed -i "s|{{ SERVER_PORT }}|${SERVER_PORT}|g" $TEMPLATE
 sed -i "s|{{ SERVER_ADDRESS }}|${SERVER_ADDRESS}|g" $TEMPLATE
 if [ ! -z "$REDIS_PORT_6379_TCP_ADDR" ]; then
-	echo "Detected link to a redis container. Using as driver"
-	if [ ! -z "$REDIS_ENV_REDIS_PASS" ]; then
-		sed -i "s|{{ DRIVER }}|redis://:${REDIS_ENV_REDIS_PASS}@${REDIS_PORT_6379_TCP_ADDR}:${REDIS_PORT_6379_TCP_PORT:-6379}/${REDIS_DB:-0}|g" $TEMPLATE
+	REDIS_HOST=$REDIS_PORT_6379_TCP_ADDR
+	REDIS_PORT=$REDIS_PORT_6379_TCP_PORT
+	REDIS_PASSWORD=$REDIS_ENV_REDIS_PASS
+elif [ ! -z "$REDIS_1_PORT_6379_TCP_ADDR" ]; then
+	# Fig link
+	REDIS_HOST=$REDIS_1_PORT_6379_TCP_ADDR
+	REDIS_PORT=$REDIS_1_PORT_6379_TCP_PORT
+	REDIS_PASSWORD=$REDIS_1_ENV_REDIS_PASS
+fi
+
+if [ ! -z "$REDIS_HOST" ]; then
+	echo "Detected Redis server. Using as driver"
+	if [ ! -z "$REDIS_PASSWORD" ]; then
+		sed -i "s|{{ DRIVER }}|redis://:${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT:-6379}/${REDIS_DB:-0}|g" $TEMPLATE
 	else
-		sed -i "s|{{ DRIVER }}|redis://${REDIS_PORT_6379_TCP_ADDR}:${REDIS_PORT_6379_TCP_PORT:-6379}/${REDIS_DB:-0}|g" $TEMPLATE
+		sed -i "s|{{ DRIVER }}|redis://${REDIS_HOST}:${REDIS_PORT:-6379}/${REDIS_DB:-0}|g" $TEMPLATE
 	fi
 else
 	sed -i "s|{{ DRIVER }}|${DRIVER}|g" $TEMPLATE
